331ac29f4e920cf8bd41ff18f8662c70f51c936d
Alkchan <alkanhassan@hotmail.com>
2022-10-02 17:20:22 +0100

    gh

---
+++ nihon35/menu.php
@@ -0,0 +1,12 @@
+<div id="side">
+    <img src="arrowright.png" class="arrow">
+    <ul>
+    <li><a href="index.htm"><span>home</span></a></li>
+    <li><a href="tokyo.php"><span class="first">TOKYO METROPOLIS</span></a></li>
+    <li><a href="kansai.php"><span class="second">kansai region</span></a></li>
+    <li><a href="west.php"><span class="third">japan west</span></a></li>
+    <li><a href="deleted1.php"><span class="fourth">deleted scenes 1</span></a></li>
+    <li><a href="deleted2.php"><span class="fifth">deleted scenes 2</span></a></li>
+    <li><a href="about.php"><span class="sixth">about</span></a></li>
+    </ul>
+</div>
---
+++ nihon35/west.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Nihon Thirty Five</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1" />
+<meta name="Keywords" content="Alkan, Hassan, Photography, Street Photography, 35mm, Film, Japan" /> 
+<meta name="Description" content="Nihon Thirty Five - Japan Street Photography on 35mm Film" />
+
+<style type="text/css">
+<!--
+li span.third{
+	border-bottom:1px solid #ddd;
+	font-weight:bold;
+}
+-->
+</style>
+
+</head>
+
+<body class="fixed">
+
+<div id="header"><img src="smalltitle.png" alt="">
+	<span>japan west</span>
+</div>
+
+<?php
+include "menu.php"
+?>
+    
+<div id="imagerow">
+	<img src="west/1.jpg" alt="" /><img src="west/2.jpg" alt="" /><img src="west/3.jpg" alt="" /><img src="west/4.jpg" alt="" /><img src="west/5.jpg" alt="" /><img src="west/6.jpg" alt="" /><img src="west/7.jpg" alt="" /><img src="west/8.jpg" alt="" /><img src="west/9.jpg" alt="" /><img src="west/10.jpg" alt="" />
+	</div>
+
+<div id="social">
+	<a href="http://twitter.com/intent/tweet?url=http://www.nihon35.com/west.php" onClick="target='_blank';"><span class="transition">share on twitter</span><img class="transition" src="twitterlogo.png" alt=""></a><a href="http://www.facebook.com/share.php?u=http://www.nihon35.com/west.php" onClick="target='_blank';"><img class="transition" src="facebooklogo.png" alt=""><span class="transition">share on facebook</span></a>
+</div>
+
+
+<script type="text/javascript" src="css3-mediaqueries.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="modernizr.js"></script>
+<script type="text/javascript" src="loader.js"></script>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16860266-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+</body>
+
+</html>

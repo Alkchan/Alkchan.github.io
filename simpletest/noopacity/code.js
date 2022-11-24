c0188c909f7bec0d84ed4ec0a10c9ea99c4242fa
Alkchan <alkanhassan@hotmail.com>
2022-11-24 14:53:56 +0000

    noopacity

---
+++ simpletest/noopacity/code.js
@@ -0,0 +1,57 @@
+		const urlString = window.location.search;
+		const urlParams = new URLSearchParams(urlString);
+		var catagory = urlParams.get('c');
+
+
+		
+		// set defaults if no urlParams exist
+		if (catagory == null)
+		{
+			catagory = 1;
+		}
+		
+		
+		if (catagory == 1)
+		{
+			document.getElementById("first").classList.toggle("displayNone");
+			
+			setTimeout(function(){
+				//document.getElementById("first").classList.toggle("opacityNone");
+				
+			}, 20);	
+		}
+		
+		if (catagory == 2)
+		{
+			document.getElementById("second").classList.toggle("displayNone");
+			
+			setTimeout(function(){
+				//document.getElementById("second").classList.toggle("opacityNone");
+			}, 20);	
+			
+		}
+
+
+
+		function show()
+		{
+			//document.getElementById("first").classList.toggle("opacityNone");
+			
+			setTimeout(function(){
+				window.location.href = "index.html?c=2";
+			}, 20);	
+			
+			
+		}
+		
+		function hide()
+		{
+			//document.getElementById("second").classList.toggle("opacityNone");
+			
+			setTimeout(function(){
+				window.location.href = "index.html?c=1";
+			}, 20);	
+		}
+
+			
+		
